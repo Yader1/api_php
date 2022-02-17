6c46afa108249788f6041ce9687b0abfbc91d959
Yader Campbell <yadermejiac123@gmail.com>
2022-02-17 11:46:06 -0600

    Configuraciones

--- auth.php
+++ auth.php
@@ -9,9 +9,26 @@
     //Consultamos que metodos esta utilizando para acceder
 
     if($_SERVER['REQUEST_METHOD'] == "POST"){
+        //Recibir datos
         $postBody = file_get_contents("php://input");
+
+        //Enviamos los datos al manejador
         $datosArray = $_auth->login($postBody);
+
+        //Devolvemos una respuesta
+        header('Content-Type: application/json');
+
+        if(isset($datosArray["result"]["error_id"])){
+            $responseCode = $datosArray["result"]["error_id"];
+            http_response_code($responseCode);
+        }else{
+            http_response_code(200);
+        }
+
+        echo json_encode($datosArray);
     }else{
-        echo "Metodo no permitido";
+        header('Content-Type: application/json');
+        $datosArray = $_respuestas->error_405();
+        echo json_encode($datosArray);
     }
 ?>
